4b6dceda8aeea0e5bcf3fc7f0e3d636ff3e6a96b
aobrazovskaya <anna.obrazovskaya@gmail.com>
2020-04-17 20:28:21 +0300

    Добавил стили главного экрана для десктопной версии

--- gulpfile.js
+++ gulpfile.js
@@ -81,7 +81,7 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**",
+    "source/js/*.js",
     "source/*.ico"
   ], {
     base: "source"
