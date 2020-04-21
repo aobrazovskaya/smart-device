55847dba9031e07ae1e17afcd5ea93b6bf03fdef
aobrazovskaya <anna.obrazovskaya@gmail.com>
2020-04-21 22:33:56 +0300

    Исправляет шапку страницы

--- build/js/main.js
+++ build/js/main.js
@@ -5,20 +5,24 @@
   var closeButton = modal.querySelector('.form__close-button');
   var nameInput = modal.querySelector('input[name="name"]');
   var modalElements = document.querySelectorAll('.form__fields > [name]');
+  var body = document.querySelector('body');
 
   orderCall.addEventListener('click', function (event) {
     event.preventDefault();
     modal.classList.remove('visually-hidden');
+    body.style['overflow-y'] = 'hidden';
     nameInput.focus();
   });
 
   closeButton.addEventListener('click', function () {
     modal.classList.add('visually-hidden');
+    body.style['overflow-y'] = 'auto';
   });
 
   modal.addEventListener('click', function (event) {
     if (event.target === event.currentTarget) {
       modal.classList.add('visually-hidden');
+      body.style['overflow-y'] = 'auto';
     }
   });
 
